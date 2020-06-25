dc602a0bc5664b8c77784a339f64d7818573e2d1
Autobuild <autobuild@example.com>
2020-06-25 11:00:17 +0000

    BUILD 147322755

---
+++ precache-manifest.8046c9a1653463b82ffe115d0c047b74.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92a53fa21c2def458477d7f3a22dd438",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8cf58f409c5b0c05877d",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d5c47f7c1414b411d227",
+    "url": "./static/css/main.3d8f9a51.chunk.css"
+  },
+  {
+    "revision": "8cf58f409c5b0c05877d",
+    "url": "./static/js/2.e1908575.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e1908575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5c47f7c1414b411d227",
+    "url": "./static/js/main.0b1419ec.chunk.js"
+  },
+  {
+    "revision": "13232cde50772fcbc943",
+    "url": "./static/js/runtime-main.273c88a2.js"
+  },
+  {
+    "revision": "03ee3124671c13d1b05b19b971cf226e",
+    "url": "./static/media/world-map-vector.03ee3124.svg"
+  }
+]);
